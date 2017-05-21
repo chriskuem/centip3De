6cfe2a72255fc10ce72fa79714a3b972a3e3e525
Christopher Kümmel <kuemmel.christopher@gmail.com>
2017-05-21 11:06:47 +0200

    add CentipedeAnimation Script

---
+++ Assets/Scripts/Centipede/CentipedeAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CentipedeAnimation : MonoBehaviour {
+
+	public float speed = 2f;
+    public float maxRotation = 45f;
+
+	public char axis = 'x';
+
+	private Quaternion curRotation = Quaternion.identity;
+
+	// Use this for initialization
+	void Start () {		
+		curRotation = gameObject.transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (axis == 'x') transform.rotation = Quaternion.Euler(curRotation.eulerAngles.x - (maxRotation * Mathf.Sin(Time.time * speed)), curRotation.eulerAngles.y, curRotation.eulerAngles.z);
+		else if (axis == 'y') transform.rotation = Quaternion.Euler(curRotation.eulerAngles.x, curRotation.eulerAngles.y - (maxRotation * Mathf.Sin(Time.time * speed)), curRotation.eulerAngles.z);
+		else if (axis == 'z') transform.rotation = Quaternion.Euler(curRotation.eulerAngles.x, curRotation.eulerAngles.y, curRotation.eulerAngles.z - (maxRotation * Mathf.Sin(Time.time * speed)));
+	}
+}
